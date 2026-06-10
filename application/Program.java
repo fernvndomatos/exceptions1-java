ee5d8c7c30c4b56bb5003fd93fba72fc341360b9
Fernando Matos <fernando.matos221@gmail.com>
2026-06-10 19:17:14 -0300

    Very bad solution

---
+++ application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import model.entites.Reservation;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Program {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        LocalDate checkIn = LocalDate.parse(sc.next(),fmt);
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        LocalDate checkOut = LocalDate.parse(sc.next(),fmt);
+
+        if(!checkOut.isAfter(checkIn)){
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }
+        else {
+            Reservation reservation = new Reservation(number,checkIn,checkOut);
+            System.out.println(reservation);
+
+            System.out.println();
+            System.out.println("Enter date to update the reservation: ");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = LocalDate.parse(sc.next(),fmt);
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = LocalDate.parse(sc.next(),fmt);
+
+            LocalDate now =  LocalDate.now();
+            if(checkIn.isBefore(now) || checkOut.isBefore(now)){
+                System.out.println("Error in reservation: Reservation dates for update must be future dates");
+            }
+            else if(!checkOut.isAfter(checkIn)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            }else {
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println(reservation);
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ model/entites/Reservation.java
@@ -0,0 +1,65 @@
+package model.entites;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public Duration duration(){
+        Duration diff = Duration.between(checkIn.atStartOfDay(), checkOut.atStartOfDay());
+        return diff;
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Reservation: Room "
+                + roomNumber
+                +", check-in: "
+                + fmt.format(checkIn)
+                +", check-out: "
+                +fmt.format(checkOut)
+                +", "
+                + duration().toDays()
+                +" nights";
+
+    }
+
+}
+
